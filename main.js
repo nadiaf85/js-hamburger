cb3885f23a2bf672beb20af12bec8a42c36810f0
nadiaf85 <nadia.fiorio@alice.it>
2021-12-09 16:29:36 +0100

    push

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const elementComparsa = document.querySelector("fa-bars");
 const elementScomparsa = document.querySelector(".close");
-const elementHamburger = document.querySelector("hamburger-menu");
+const elementHamburger = document.querySelector(".hamburger-menu");
 
 elementComparsa.addEventListner('click',function(){
     elementHamburger.classList.add("active");
