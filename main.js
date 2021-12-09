e425cdc761110ed5ece35caedb1cf977434c8357
nadiaf85 <nadia.fiorio@alice.it>
2021-12-09 16:22:44 +0100

    modifiche

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const elementComparsa = document.querySelector("fa-bars");
-const elementScomparsa = document.querySelector("close");
+const elementScomparsa = document.querySelector(".close");
 const elementHamburger = document.querySelector("hamburger-menu");
 
-elementComparsa.addEventListner("click",function(){
+elementComparsa.addEventListner('click',function(){
     elementHamburger.classList.add("active");
 });
 
-elementScomparsa.addEventListner("click",function(){
+elementScomparsa.addEventListner('click',function(){
     elementHamburger.classList.remove("active");
 });
